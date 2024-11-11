e872e88915ad1bbb9df14c0989a5ef099736c932
caireuy <croxas@academic.rrc.ca>
2024-11-10 21:21:07 -0600

    Initial commit: Set up project directory and file structure.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Assignment 6
+
+## Description
+
+Module 6
+
+## Author
+
+Carey Roxas
+
+## Assignment
+
+This project consists of the learning outcome of Module 6: Classes, Enumerations
---
+++ mortgage/mortgage.py
@@ -0,0 +1,9 @@
+"""
+Description: A class meant to manage Mortgage options.
+Author: Carey Roxas
+Date: Novemeber 10, 2024
+Usage: Create an instance of the Mortgage class to manage mortgage records and 
+calculate payments.
+"""
+
+
