ac4ce8d8035bf366496ba5721e17c86f85c2b3c4
caireuy <croxas@academic.rrc.ca>
2024-11-10 22:02:25 -0600

    Added docstring for class Mortgage in mortgage.py 2

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -6,4 +6,13 @@ Usage: Create an instance of the Mortgage class to manage mortgage records and
 calculate payments.
 """
 
+from mortgage.pixell_lookup import MortgageRate, PaymentFrequency, VALID_AMORTIZATION
 
+class Mortgage:
+    """
+    Class to represent the MortgageRate, PaymentFrequency and the valid amortization
+
+    MortgageRate: values of mortgage rate
+    PaymentFrequency: payment frequency options
+    VALID_AMORTIZATION: list of valid years for amortization
+    """
