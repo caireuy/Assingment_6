ef9863e9d90785d1698e904dd216f42dea0398ab
caireuy <croxas@academic.rrc.ca>
2024-11-17 11:25:48 -0600

    Defined Mortgage class in mortgage.py

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -16,3 +16,41 @@ class Mortgage:
     PaymentFrequency: payment frequency options
     VALID_AMORTIZATION: list of valid years for amortization
     """
+
+    ##__INIT__
+    def __init__(self, loan_amount: float, rate: str, frequency: str, amortization: int):
+        """
+        Initialize Mortgage validation with attributes: Loan amouunt, rate, frequency, amortization.
+
+        Arg:
+            Loan_amount(float): The amount of the mortgage loan.
+            Rate(str): The annual interest rate string equivalent to enum value
+            Frequency(str): The number of payments per year string equivalent to enum value.
+            Amortization(int): The number of years to repay the mortgage loan.
+
+        Raises:
+            ValueError:  if the loan amount value is zero or negative
+            ValueError: if the rate provided is invalid
+            ValueError: if the frequency provided is invalid
+            ValueError: if the amortization is invalid   
+        """
+        # Loan_amount validation
+        self.__loan_amount = loan_amount
+        if loan_amount <= 0:
+            raise ValueError("Loan Amount must be positive")
+        
+        # Rate Validation
+        try:
+            self.__rate = MortgageRate[rate]
+        except Exception as e:
+            raise ValueError("Rate provided is invalid.")
+        
+        # Frequency Validation
+        try:
+            self.__frequency = PaymentFrequency[frequency]
+        except Exception as e:
+            raise ValueError("Frequency provided is invalid.")
+        
+        #
+
+
--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -14,38 +14,44 @@ from enum import Enum
 
 VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
 
-def MortgageRate(Enum):
+class MortgageRate(Enum):
     """
-    Represents the Mortage rate values
+    Represents the Mortgage rate values
+    
+    Values:
+        FIXED_5: Fixed mortgage rate 5th value
+        FIXEED_3: FIxed mortgage rate 3rd value
+        FIXED_1: Fixed mortgage rate 1st value
+        VARIABLE_5: 5th  variable for mortgage rate
+        VARIABLE_3: 3rd variable for mortgage rate
+        VARIABLE_1: 1st variable for mortgage rate     
     """
 
     FIXED_5: 0.0519
-    """Fixed mortage rate 5th value"""
 
     FIXED_3: 0.0589
-    """FIxed mortage rate 3rd value"""
 
     FIXED_1: 0.0599 
-    """Fixed mortgage rate 1st value"""
 
     VARIABLE_5: 0.0649 
-    """5th  variable for mortgage rate"""
 
     VARIABLE_3: 0.0669 
-    """3rd variable for mortgage rate"""
 
     VARIABLE_1: 0.0679 
-    """1st variable for mortgage rate"""
     
-def PaymentFrequency(Enum):
+    
+class PaymentFrequency(Enum):
     """
     Represents the payment frequency options
+    
+    Values:
+        MONTHLY: Monthly payment frequency of 12 times a year
+        BI_WEEKLY: Bi-weekly Payment frequency of 26 times a year
+        WEEKLY: Weekly payment frequency of 52 times a year
     """
     MONTHLY: 12
-    """Monthly payment frequency of 12 times a year"""
 
     BI_WEEKLY: 26
-    """Bi-weekly Payment frequemcy of 26 times a year"""
 
     WEEKLY: 52
-    """Weekly payment frequency of 52 times a year"""
+ 
